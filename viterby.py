011e765b7a2e508d976565e3770d0073e76d668f
victorMijangos <VMijangosC@iingen.unam.mx>
2016-02-18 11:30:25 -0600

    Update viterby.py

--- viterby.py
+++ viterby.py
@@ -4,27 +4,33 @@ from pickle import load
 from operator import itemgetter
 import numpy as np
 
+#Importa el modelo de entrenamiento
 A,B,Pi,S,O = load(open('HMM.p','r'))
 
+#Texto a analizar
 text = raw_input('INP: ').split() #.encode('utf_8').split()
 
+#Algoritmo de Viterbi
 d = 0
 tags = []
 for k in range(len(text)):
 	try:
+		#Asigna el vector de probabilidades a una observacion
 		obs = B[O[text[k]]]
 	except:
-		obs = np.ones(len(S))*0.001
+		#Si no encuentra alguna palabra en los datos de entrenamiento
+		obs = np.ones(len(S))
 	if k == 0:
+	#Inicialización
 		phi,d = max( {t:Pi[i]*obs[i] for t,i in S.items()}.iteritems(), key=itemgetter(1) )
 		tags.append(phi)
-#		print text[k],phi,d
 	else:
+	#Induccion
 		ant = S[phi]
 		phi,d = max( {t:d*A[ant][i]*obs[i] for t,i in S.items()}.iteritems(), key=itemgetter(1) )
 		tags.append(phi)
-#		print text[k],phi,d
 
+#Imprime en formato JSON
 print '[',
 for j,w in enumerate(text):
 	print '[{"token":"%s",' % w,
